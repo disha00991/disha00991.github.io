4e13973fb529cb2cf65f0c16e1e395276b8da7ec
dishasingh <disha@thb.co.in>
2019-10-20 11:41:35 +0530

    Updates

---
+++ precache-manifest.70cec737456cc574bc7aa7cee7d1f27c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6599dab73ad464aa24c884f341d44524",
+    "url": "/webume/index.html"
+  },
+  {
+    "revision": "5f4bfafa5a4f5219e135",
+    "url": "/webume/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "e9209c199f8ec6e4253e",
+    "url": "/webume/static/css/main.76ed0770.chunk.css"
+  },
+  {
+    "revision": "5f4bfafa5a4f5219e135",
+    "url": "/webume/static/js/2.c4899402.chunk.js"
+  },
+  {
+    "revision": "e9209c199f8ec6e4253e",
+    "url": "/webume/static/js/main.e899dece.chunk.js"
+  },
+  {
+    "revision": "799678427f078beb789f",
+    "url": "/webume/static/js/runtime-main.6e8e1a60.js"
+  }
+]);
