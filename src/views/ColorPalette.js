b03dd7f7c8bf5d9d9270c149d84e57bc436c4ff0
disha00991 <directionstar86@gmail.com>
2021-07-21 23:24:04 -0400

    added n_colors

--- src/views/ColorPalette.js
+++ src/views/ColorPalette.js
@@ -15,10 +15,10 @@ const Projects = () => (
                     <p>A project based on Clustering methods in Machine Learning</p>
                 </div>
                 <div className="title col-6">
-                    <img width="120px" src={process.env.PUBLIC_URL+'/images/palet.png'}/>
+                    <img width="120px" src={process.env.PUBLIC_URL + '/images/palet.png'} />
                 </div>
             </header>
-            <p><strong>About Project:</strong> This project uses one of the methods in unsupervised learning to cluster the different colors in
+            <p><strong>About Project:</strong> This project shows how unsupervised learning can be used to cluster the different colors in
                 a picture to create a color palette. The palette can be used to create a gradient background for the picture or to select font color
                 for any text to write on or around it!</p>
             <ClusteringForm />
@@ -30,60 +30,66 @@ const Projects = () => (
 export function ClusteringForm(props) {
     const [algo, setAlgo] = useState("KMeans");
     const [url, setUrl] = useState("");
+    const [n_colors, setNoOfColors] = useState(6);
     const algoOptions = ['KMeans', 'KMeans++', 'Heirarchical Clustering', 'DAG Clustering', 'Linear Discriminant(LDA)', 'Kernel PCA']
-    const [response, setResponse] = useState("")
+    const [palette, setPalette] = useState("")
     const [error, setError] = useState("");
     const [loading, setLoading] = useState(false);
 
     const handleSubmit = (evt) => {
         evt.preventDefault();
-        callAPI(algo, url)
+        callAPI()
     }
 
     const callAPI = () => {
         setLoading(true)
-        axios.post('https://color-palette-creation.herokuapp.com/get_color_palette', {
-            data: {
-                url: url,
-                is_url: 'true',
-                algo: algo
-            },
+        let queryParams = `is_url=${true}&url=${url}&algo=${algo}&n_colors=${n_colors}`
+        axios.post('https://color-palette-creation.herokuapp.com/get_color_palette?' + queryParams, {
             headers: {
                 'Access-Control-Allow-Origin': '*'
             }
-        })
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    // response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
-                    console.log("disha:"+result)
-                    setResponse(result)
-                },
-                (error) => {
-                    setLoading(false);
-                    setError(error);
-                }
-            )
+        }).then(
+            (response) => {
+                let palette = (response.data).map(rgb => `rgb(${rgb[0]},${rgb[1]},${rgb[2]})`)
+                setPalette(palette)
+                setLoading(false);
+                console.log(palette)
+            },
+            (error) => {
+                setLoading(false);
+                setError(error);
+            }
+        )
     }
 
     return (
         <>
             <form onSubmit={handleSubmit} className="container row">
                 <label className="col-3 pd-0" >
-                    Algorithm to use:
+                    Algorithm to use*:
                     <select name="algo" id="algo" value={algo} onChange={e => setAlgo(e.target.value)}>
                         {algoOptions && algoOptions.map(al => <option value={al}>{al}</option>)}
                     </select>
                 </label>
-                <label className="col-9">
-                    Image URL
-                    <small style={{ 'color': 'grey'}}> For eg: https://d21zeai4l2a92w.cloudfront.net/wp-content/uploads/2020/01/ColorChangingFlowers.jpg</small>
+                <label className="col-2" >
+                    No. of colors*:
+                    <input type="number" min="1" name="n_colors" id="n_colors" value={n_colors} onChange={e => setNoOfColors(e.target.value)}/>
+                </label>
+                <label className="col-7">
+                    Image URL*:
+                    <small style={{ 'color': 'grey' }}> For eg: https://d21zeai4l2a92w.cloudfront.net/wp-content/uploads/2020/01/ColorChangingFlowers.jpg</small>
                     <input type="text" name="url" id="url" value={url} onChange={e => setUrl(e.target.value)} />
                 </label>
-                <input type="submit" value="Create Palette!" disable={!url || !algo}/>
+                {algo && url && n_colors && <input type="submit" value="Create Palette!" disable={!url || !algo} />}
+                {loading && <div style={{ display: 'inline', marginLeft: '15px' }}><Spinner height={40} width={40}></Spinner></div>}
             </form>
             {error && <small style={{ 'color': 'red' }}>Some error occurred!</small>}
-            {loading && <div style={{ display: 'inline', marginLeft: '15px' }}><Spinner height={40} width={40}></Spinner></div>}
+            {palette && url && <div className="palette-container">
+                <div className="mb-10">
+                    {palette.map(clr => <div className="palette-color d-inline-block" style={{ backgroundColor: clr }}></div>)}
+                </div>
+                <img src={url} width="400"/>
+            </div>}           
         </>
     );
 }
